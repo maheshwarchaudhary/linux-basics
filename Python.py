9d50ed763e04cb1c56162bd65c1d034571d7fbf4
maheshwarchaudhary <mchaudhary962@rku.ac.in>
2025-12-19 17:58:54 +0000

    add Basic_Object

---
+++ Python.py
@@ -0,0 +1,19 @@
+#List
+mylist = [1,2,3,4,5]    
+print(mylist)
+print(type(mylist))
+
+#Dictionary
+mydict = {"name":"John", "age":30, "city":"New York"}
+print(mydict)           
+print(type(mydict))
+
+#Set
+myset = {1,2,3,4,5}     
+print(myset)
+print(type(myset))
+
+#Tuple
+mytuple = (1,2,3,4,5)
+print(mytuple)  
+print(type(mytuple))
